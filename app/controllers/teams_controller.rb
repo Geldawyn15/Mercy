07552d5c8c0cb25d32ac6551a7bb32cb42ce2612
JonasChancelier <Jonaschancelier@gmail.com>
2019-05-29 10:52:16 +0200

    Controller-setup

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,24 @@
 class TeamsController < ApplicationController
   def show
+    @team = Team.new
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(params_team)
+    if @team.save!
+      redirect_to user_teams_path(@users)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_team
+    params.require(:teams).permit(:spirit, :rank_scale, :status)
   end
 end
--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -1,7 +1,24 @@
 class UserReviewsController < ApplicationController
   def new
+    @review = UserReview.new
   end
 
   def create
+    @review = UserReview.new(params_review)
+    if @review.save!
+      redirect_to user_profiles_path(@users)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_review
+    if :nok == true
+      params.require(:user_reviews).permit(:nok_positiveness, :nok_respect, :nok_helpfulness, :nok_communication)
+    else
+      params.require(:user_reviews).permit(:endorse, :add_friend)
+    end
   end
 end
