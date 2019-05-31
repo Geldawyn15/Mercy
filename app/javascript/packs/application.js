ef6a4953f6a0c7b87f4f939161d7d9e8a88f223c
Alexandre Chelednik <cheleds15@gmail.com>
2019-05-31 16:24:42 +0200

    for demo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,3 @@
 import "bootstrap";
-import Glide from '@glidejs/glide';
-import '@glidejs/glide/dist/css/glide.core.min.css'
-import * from '../tooltip.js'
 
-new Glide('.glide', {
-  type: 'carousel',
-  startAt: 0,
-  perView: 4
-}).mount()
 
