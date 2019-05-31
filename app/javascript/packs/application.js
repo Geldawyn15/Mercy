67213866922d98b07da7c69f5032798083ad2dba
Alexandre Chelednik <cheleds15@gmail.com>
2019-05-31 15:45:44 +0200

    toto

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import Glide from '@glidejs/glide';
 import '@glidejs/glide/dist/css/glide.core.min.css'
+import * from '../tooltip.js'
 
 new Glide('.glide', {
   type: 'carousel',
---
+++ app/javascript/tooltips.js
@@ -0,0 +1,10 @@
+import PopperJs from 'popper.js';
+
+referenceElement = document.querySelector(".deg0")
+  new Tooltip(referenceElement, {
+    placement: 'top',
+    title: "Person"
+  });
+
+
+export * from "tooltip.js"
