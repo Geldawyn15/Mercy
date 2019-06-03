5770b5489831434b23fadb791478357f455beb87
Alexandre Chelednik <cheleds15@gmail.com>
2019-06-03 09:36:56 +0200

    toto

---
+++ app/javascript/manageMates.js
@@ -0,0 +1,5 @@
+const manageMates = () => {
+
+}
+
+export { gameSelect };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { gameSelect } from "../gameSelect.js";
+import { manageMates } from "../manageMates.js";
 
 gameSelect();
+manageMates();
 
 
