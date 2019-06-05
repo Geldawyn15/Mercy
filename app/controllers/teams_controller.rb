1f6a4a81ab95fff963129532a53edd960e0459e1
Clotilde <clotildemathieu@hotmail.com>
2019-06-05 13:00:51 +0200

    flipcard design OK

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -30,8 +30,6 @@ class TeamsController < ApplicationController
   def review
     @team_review = TeamReview.new
     @user_review = UserReview.new
-    @ov = OverwatchCharacterScrapper.new
-    @ov_img = @ov.scrapping2
   end
 
   private
